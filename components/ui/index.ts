e5dbe2a33cb09fdd043fbaaac6c8c9247d5c71ae
Sujay Mohithe <sujaymohithe@outlook.com>
2026-01-15 12:35:27 +0100

    feat: implemented contact page

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -2,4 +2,5 @@ export { Button } from "./Button";
 export { OverviewRow } from "./Overview";
 export { Chip } from "./Chip";
 export { List } from "./List";
+export { SmartLink } from "./SmartLink";
 
